157808d188229a6f3d59ff98cba67a8f91f60971
Neeraj Kumar <neerajpkumar@gmail.com>
2015-10-24 14:14:13 -0700

    Making barcode scanner a class

--- detect_calories.py
+++ detect_calories.py
@@ -2,36 +2,34 @@
 import sys
 import time
 
-fp = open('/dev/hidraw0', 'rb')
 
+class BarcodeScanner():
+	def __init__(self, channel="/dev/hidraw0"):
+		self.fp = open(channel, 'rb')
 
-def read_next_barcode():
-  barcode = ''
-  while True:
-    if barcode == '':
-       time.sleep(0.1)
-    buffer = fp.read(8)
-    for c in buffer:
-      num_val = ord(c)
-      if num_val == 40:
-        return barcode
-      else:
-        if num_val > 29 and num_val < 40:
-          barcode += str((num_val +1) % 10)
-  
+	def read_next_barcode(self):
+		barcode = ''
+		while True:
+			if barcode == '':
+				time.sleep(0.1)
+			buffer = self.fp.read(8)
+			for c in buffer:
+				num_val = ord(c)
+				if num_val == 40:
+					return barcode
+				else:
+				if num_val > 29 and num_val < 40:
+					barcode += str((num_val +1) % 10)
 
-def read_weight():
-  print "yet to be implemented"
-
-
-try:
-  while 1:
-    print 'reading next'
-    print read_next_barcode()
-except KeyboardInterrupt:
-  print 'Exiting...'
-  sys.exit(0)
-
+if __name__ == "__main__":
+	scanner = BarcodeScanner()
+	try:
+	  while 1:
+		print 'reading next'
+		print scanner.read_next_barcode()
+	except KeyboardInterrupt:
+	  print 'Exiting...'
+	  sys.exit(0)
 
 
 
