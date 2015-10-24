f95e26486f8405c43c64a589ced8463393731df7
Neeraj Kumar <neerajpkumar@gmail.com>
2015-10-24 14:20:04 -0700

    1. Moving barcode scanner into a new file. 2. Using required classes in detect_calories

--- detect_calories.py
+++ detect_calories.py
@@ -1,25 +1,7 @@
-
 import sys
 import time
-
-
-class BarcodeScanner():
-	def __init__(self, channel="/dev/hidraw0"):
-		self.fp = open(channel, 'rb')
-
-	def read_next_barcode(self):
-		barcode = ''
-		while True:
-			if barcode == '':
-				time.sleep(0.1)
-			buffer = self.fp.read(8)
-			for c in buffer:
-				num_val = ord(c)
-				if num_val == 40:
-					return barcode
-				else:
-				if num_val > 29 and num_val < 40:
-					barcode += str((num_val +1) % 10)
+from barcode_scanner import BarcodeScanner
+from usbscale import Scale
 
 if __name__ == "__main__":
 	scanner = BarcodeScanner()
