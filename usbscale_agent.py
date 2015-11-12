63e08de2a1f0044a134e15d8d21f8185847d7d89
Neeraj Kumar <neerajpkumar@gmail.com>
2015-11-12 00:49:47 +0000

    Fixing issues with usb scale agent

--- usbscale_agent.py
+++ usbscale_agent.py
@@ -7,6 +7,7 @@ address = ('localhost', 6000)
 
 def send_weight_to_blackboard(weight):
     conn = Client(address)
+    print("Sending weight :" + str(weight))
     conn.send('PUT:weight,' + str(weight))
     conn.send('close')
     conn.close()
@@ -17,11 +18,15 @@ if __name__ == "__main__":
     history = list()
     while True:
         weight = scale.get_weight()
+        #print("Received weight : " + str(weight))
         history.append(weight)
+        #print(history)
         if len(history) > 6:
-            history.pop()
-        send_weight_to_blackboard(max(history))
-        time.sleep(0.5)
+            del history[0]
+        max_weight = max(history)
+        #print("Max weight : " + str(max_weight))
+        send_weight_to_blackboard(max_weight)
+        time.sleep(0.1)
   except KeyboardInterrupt:
     print("Exiting...")
   scale.cleanup()
