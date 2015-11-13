799f560dd564452fe02684c33e86bb55e6fe1453
Neeraj Kumar <neerajpkumar@gmail.com>
2015-11-13 14:45:07 -0800

    Adding bluetooth scanner

--- bluetooth_scanning.py
+++ bluetooth_scanning.py
@@ -1,13 +1,21 @@
 from __future__ import print_function
+from multiprocessing.connection import Client
+
 import bluetooth
 import time
 
+address = ('localhost', 6000)
 
 def write_to_file(nearby_devices):
   with open("devices.txt", "w") as f:
     for device in nearby_devices:
       print(device)
-    
+
+def update_blackboard(nearby_devices):
+    conn = Client(address)
+    conn.send('PUT:devices,' + str(nearby_devices))
+    conn.send('close')
+    conn.close()
 
 while True:
   nearby_devices = bluetooth.discover_devices(lookup_names = False, duration=2)
