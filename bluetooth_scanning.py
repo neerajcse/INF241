3e1d69768938247a2afc0b273cfdfd6fe00cd1a0
Neeraj Kumar <neerajpkumar@gmail.com>
2015-11-13 15:02:04 -0800

    Adding user details from bluetooth

--- bluetooth_scanning.py
+++ bluetooth_scanning.py
@@ -5,6 +5,7 @@ import bluetooth
 import time
 
 address = ('localhost', 6000)
+user1_devices = ['60:6C:66:03:04:ED']
 
 def write_to_file(nearby_devices):
   with open("devices.txt", "w") as f:
@@ -13,7 +14,12 @@ def write_to_file(nearby_devices):
 
 def update_blackboard(nearby_devices):
     conn = Client(address)
-    conn.send('PUT:devices,' + str(nearby_devices))
+    user = "2"
+    for device in nearby_devices:
+        if device in user1_devices:
+            user = "1"
+            break
+    conn.send('PUT:user,' + str(user))
     conn.send('close')
     conn.close()
 
